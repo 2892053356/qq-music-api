24c52beb554f891a0b1cde0edcbe5971dd55343c
sansenjian <1979432414@qq.com>
2026-03-03 02:23:21 +0800

    security: 在 y_common.js 日志中屏蔽敏感 header
    - 添加 SENSITIVE_HEADER_KEYS 列表
    - 在 DEBUG 模式下屏蔽 cookie, authorization, proxy-authorization 等敏感字段

--- module/apis/y_common.js
+++ module/apis/y_common.js
@@ -11,6 +11,16 @@ module.exports = ({ url, method = 'get', options = {}, hasCommonParams = true })
 	});
 	if (process.env.DEBUG === 'true') {
 		const logOpts = { ...opts, headers: { ...opts.headers } };
+		const SENSITIVE_HEADER_KEYS = ['cookie', 'authorization', 'proxy-authorization'];
+
+		if (logOpts.headers) {
+			Object.keys(logOpts.headers).forEach(key => {
+				if (SENSITIVE_HEADER_KEYS.includes(key.toLowerCase())) {
+					logOpts.headers[key] = '[masked]';
+				}
+			});
+		}
+
 		console.log(url, { opts: logOpts });
 	}
 	return request(url, method, opts);
